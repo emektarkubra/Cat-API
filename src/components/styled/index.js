63edc9599abf9e4feacbe549b7b0b59c8ee145c1
Kübra Emektar <emektarrkubra@gmail.com>
2023-08-28 15:36:54 +0300

    added GlobalStyle

---
+++ src/components/styled/index.js
@@ -0,0 +1,7 @@
+import { StyledBadge } from "./Badge.styled";
+import { StyledCatBreedsFormBox } from "./CatBreedsForm.styled";
+import { StyledContainer } from "./Container.styled";
+import { GlobalStyle } from "./GlobalCSS";
+
+
+export { StyledBadge, StyledCatBreedsFormBox, GlobalStyle, StyledContainer };
